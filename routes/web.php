becaf14e9f24621e86ad256ab0b1f2a1503081f4
Ezgi <ezgiadas1@gmail.com>
2017-05-27 00:42:16 +0200

    Revert "php Teil von comments"
    This reverts commit 9cf5a90271e50ba5c49939b346cf4419f7de3827.

--- routes/web.php
+++ routes/web.php
@@ -18,33 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/comments', function () {
-    return view('comments');
-});
-
-
-
-Route::post('/comments',function(Request $request){
-    $comment = Comment::create($request->all());
-
-    return Response::json($comment);
-});
-
-Route::put('/comments/{comment_id?}/',function(Request $request,$comment_id){
-    $comment = Comment::find($comment_id);
-
-    $comment->message = $request->message;
-    $comment->is_flagged = $request->is_flagged;
-
-    $comment->save();
-
-    return Response::json($comment);
-});
-
-Route::delete('/comments/{comment_id?}',function($comment_id){
-    $comment = Comment::destroy($comment_id);
-
-    return Response::json($comment);
-});
-
