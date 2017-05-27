569f2a7d0e6b51e1fd0003a00394f6707901d43a
Cemre Yüksel <cemreyuksel95@gmail.com>
2017-05-27 13:59:53 +0200

    fillable fix

--- app/Meme.php
+++ app/Meme.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Meme extends Model
 {
     //
-    protected $fillable = ['is_flagged', 'upvotes','downvotes', 'image_url', 'title', 'user_id', 'group_id'];
+    protected $fillable = ['votes', 'title'];
 }
--- app/MemeUserVote.php
+++ app/MemeUserVote.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class MemeUserVote extends Model
 {
-    protected $fillable = ['vote','meme_id', 'user_id'];
+    protected $fillable = ['vote'];
 }
